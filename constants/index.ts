3a2a69c3e85fbe27159c3a1a5b31b551f5412176
Colin Franceschini <colin.franceschini@gmail.com>
2023-04-04 17:52:30 -0700

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -3,7 +3,7 @@ import { API_KEY, DEV_ENV } from "@env"
 console.log(DEV_ENV)
 let apiUrl = "https://kwallet.app"
 if (DEV_ENV === "development") {
-  apiUrl = "http://localhost:300"
+  apiUrl = "http://localhost:3000"
 }
 
 const apiKey = API_KEY
