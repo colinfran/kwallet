6ea785d46f82a30fcfffba46f56f821aeecf4655
Colin Franceschini <colin.franceschini@gmail.com>
2023-04-13 14:26:52 -0700

    code cleanup

--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,6 @@ import http from "http"
 import cookieParser from "cookie-parser"
 import bodyParser from "body-parser"
 import morgan from "morgan"
-import cron from "node-cron"
 import { fileURLToPath } from "url"
 import { initializeDatabase } from "./database/index.js"
 import * as dotenv from "dotenv"
@@ -14,40 +13,29 @@ import * as Sentry from "@sentry/node"
 import Tracing from "@sentry/tracing"
 import cors from "cors"
 import rateLimit from "express-rate-limit"
-
-import { updateData, updateCurrentPrice, sleep } from "./functions/functions.js"
 import apiRoute from "./routes/index.js"
 
 import { initKaspaFramework } from "@kaspa/wallet"
+import { initializeCronJobs } from "./cron/index.js"
 
 const app = express()
 const server = http.createServer(app)
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 dotenv.config()
-
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
   integrations: [
-    // enable HTTP calls tracing
     new Sentry.Integrations.Http({ tracing: true }),
-    // enable Express.js middleware tracing
     new Tracing.Integrations.Express({ app }),
-    // Automatically instrument Node.js libraries and frameworks
     ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
   ],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
   tracesSampleRate: 1.0,
 })
 
-// RequestHandler creates a separate execution context using domains, so that every
-// transaction/span/breadcrumb is attached to its own Hub instance
 app.use(Sentry.Handlers.requestHandler())
-// TracingHandler creates a trace for every incoming request
 app.use(Sentry.Handlers.tracingHandler())
-
 const limiter = rateLimit({
   windowMs: 10 * 60 * 1000, // 10 minutes
   max: 100, // Limit each IP to 60 requests per `window` (here, per 10 minutes)
@@ -60,9 +48,7 @@ const limiter = rateLimit({
       "Attention. You have exceeded the rate limit. You can only make 100 requests every 10 minutes.",
   },
 })
-
 app.use(limiter)
-
 app.use(
   morgan("dev", {
     skip: (req, res) => {
@@ -71,122 +57,15 @@ app.use(
   })
 )
 app.use(express.json())
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
 app.use(cors())
-
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-
 app.use(bodyParser.json())
-
 app.use(express.static("storage.json"))
-
 app.use(express.static(__dirname + "/public"))
 
 initializeDatabase()
-// initializeKaspa()
-/* 
-  Coingecko graciously provides a free public api in which we pull our 
-  data from. This server periodically refreshes its local database with 
-  Coingecko's Kaspa data.
-
-
-  International currencies supported: 
-  -usd
-  -euro
-  -japanese yen
-  -british pound
-  -chinese yuan
-  -australian dollar
-  -canadian dollar
-  -swiss franc
-
-*/
-
-const supportedCurrencies = [
-  "usd",
-  "eur",
-  "jpy",
-  "gbp",
-  "cny",
-  "aud",
-  "cad",
-  "chf",
-]
-
-// run every 8 minutes - currentPrice
-cron.schedule("*/8 * * * *", () => {
-  const run = async () => {
-    for (const currency of supportedCurrencies) {
-      await updateCurrentPrice(currency)
-      await sleep(20)
-    }
-  }
-  run()
-})
-
-// run every 15 minutes - 1D
-cron.schedule("*/15 * * * *", () => {
-  const run = async () => {
-    await sleep(23)
-    for (const currency of supportedCurrencies) {
-      await updateData("1D", currency, 23)
-      await sleep(20)
-    }
-  }
-  run()
-})
-
-// run every 30 minutes - 1W
-cron.schedule("*/30 * * * *", () => {
-  const run = async () => {
-    await sleep(33)
-    for (const currency of supportedCurrencies) {
-      await updateData("1W", currency, 33)
-      await sleep(20)
-    }
-  }
-  run()
-})
-
-// run every hour - 1M
-cron.schedule("0 * * * *", async () => {
-  const run = async () => {
-    await sleep(121)
-    for (const currency of supportedCurrencies) {
-      await updateData("1M", currency, 47)
-      await sleep(20)
-    }
-  }
-  run()
-})
-
-// run every 2 hours - 1Y
-cron.schedule("0 */2 * * *", async () => {
-  const run = async () => {
-    await sleep(77)
-    for (const currency of supportedCurrencies) {
-      await updateData("1Y", currency, 63)
-      await sleep(20)
-    }
-  }
-  run()
-})
-
-// run every 3 hours - ALL
-cron.schedule("0 */3 * * *", async () => {
-  const run = async () => {
-    await sleep(99)
-    for (const currency of supportedCurrencies) {
-      await updateData("ALL", currency, 73)
-      await sleep(20)
-    }
-  }
-  run()
-})
+initializeCronJobs()
 
 /*
   Api endpoint
