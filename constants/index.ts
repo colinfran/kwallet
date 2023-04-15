26c3e729e67334dccdadc5d6a517486e6e5d6917
Colin Franceschini <colin.franceschini@gmail.com>
2023-04-14 19:00:16 -0700

    cleanup

--- constants/index.ts
+++ constants/index.ts
@@ -1,14 +1,12 @@
-import {
-  API_KEY as apiKey,
-  DEV_ENV,
-  NETWORK_IP,
-  SENTRY_DSN as sentryDsn,
-} from "@env"
+import { API_KEY, DEV_ENV, NETWORK_IP, SENTRY_DSN } from "@env"
 
 console.log(DEV_ENV)
 let apiUrl = "https://kwallet.app"
 if (DEV_ENV === "development") {
   apiUrl = `http://${NETWORK_IP}:3000`
 }
+
+const apiKey = API_KEY
+const sentryDsn = SENTRY_DSN
 
 export { apiKey, apiUrl, sentryDsn }
