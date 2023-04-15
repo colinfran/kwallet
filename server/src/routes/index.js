52d296d014288a1f0a8c94d9bb8e6f2423f2402c
Colin Franceschini <colin.franceschini@gmail.com>
2023-04-14 21:59:04 -0700

    Update index.js

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -29,18 +29,24 @@ route.get([...privacy, ...terms, ...index], async (req, res) => {
 })
 
 /**
- * @route GET url analytics
- * @desc url for analytics
+ * @route GET url shortener for analytics
+ * @desc url shortener for analytics
  * @access Public
  * @returns {object} 200 - redirects to homepage with analytics
  * @returns {Error}  500 - Unexpected error
  */
 route.get("/k/:id", (req, res) => {
-  console.log(req.params)
   const { id } = req.params
+  const val = id.split("+")
+  let source = val[0]
+  let medium = val[0]
+  if (val.length > 1) {
+    medium = val[1]
+  }
+  // console.log(`https://kwallet.app/?utm_source=${source}&utm_medium=${medium}`)
   res.redirect(
     // eslint-disable-next-line max-len
-    `https://kwallet.app/?utm_source=${id}&utm_medium=${id}&utm_campaign=${id}`
+    `https://kwallet.app/?utm_source=${source}&utm_medium=${medium}`
   )
 })
 
