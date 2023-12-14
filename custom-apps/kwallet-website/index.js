75ad5cd667763c153d2d9d70347e91ab41a1d615
Colin Franceschini <colin.franceschini@gmail.com>
2023-12-13 20:12:34 -0800

    update

--- custom-apps/kwallet-website/index.js
+++ custom-apps/kwallet-website/index.js
@@ -12,67 +12,67 @@ window.addEventListener("load", function () {
 const url = "https://api.kwallet.app/v1/email-sign-up"
 const form = document.querySelector("#form")
 const btn = document.querySelector("#submit")
-form.addEventListener("submit", (e) => {
-  e.preventDefault()
-  const formData = new FormData(form)
-  const email = formData.get("Email")
-  if (email === undefined || email === "") {
-    return
-  }
-  var mailformat =
-    /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
-  if (!email.match(mailformat)) {
-    document.querySelector("#o3Uzv2").className = "_8rqOjx"
-    return
-  }
-  btn.disabled = true
-  btn.innerHTML = `
-    <span class="_0CvEDb">
-      <span></span>
-      <span></span>
-      <span></span>
-    </span>
-  `
-  fetch(url, {
-    method: "POST",
-    body: JSON.stringify({
-      email,
-    }),
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-  })
-    .then(function(response) { return response.json(); })
-    .then(function(json) {
-      console.log(json)
-      if (json.message === "User already signed up"){
-        console.log("error")
-        btn.disabled = true
-        btn.innerHTML = "Submit"
-        document.querySelector("#UMIFNf").style = "display:none;"
-        document.querySelector("#YH7cBX").className = "_l79chJ"
-        document.querySelector("#QwgFJH").innerHTML =
-        "You have already signed up for early access."
-        return
-      }
-      btn.disabled = true
-      btn.disabled = false
-      btn.innerHTML = "Submit"
-      document.querySelector("#YH7cBX").className = "_y89x4g"
-      document.querySelector("#QwgFJH").innerHTML =
-        "You have successfully signed up for early access for the kwallet app. When the app is ready for testing, you will receive an email from us."
-      document.querySelector("#UMIFNf").style = "display:none;"
-    })
-    .catch((error) => {
-      btn.disabled = false
-      btn.innerHTML = "Submit"
-      document.querySelector("#UMIFNf").style = "display:none;"
-      document.querySelector("#YH7cBX").className = "_l79chJ"
-      document.querySelector("#QwgFJH").innerHTML =
-        "An error occurred when trying to submit your email. Please refresh the page and try again."
-    })
-})
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault()
+//   const formData = new FormData(form)
+//   const email = formData.get("Email")
+//   if (email === undefined || email === "") {
+//     return
+//   }
+//   var mailformat =
+//     /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+//   if (!email.match(mailformat)) {
+//     document.querySelector("#o3Uzv2").className = "_8rqOjx"
+//     return
+//   }
+//   btn.disabled = true
+//   btn.innerHTML = `
+//     <span class="_0CvEDb">
+//       <span></span>
+//       <span></span>
+//       <span></span>
+//     </span>
+//   `
+//   fetch(url, {
+//     method: "POST",
+//     body: JSON.stringify({
+//       email,
+//     }),
+//     headers: {
+//       Accept: "application/json",
+//       "Content-Type": "application/json",
+//     },
+//   })
+//     .then(function(response) { return response.json(); })
+//     .then(function(json) {
+//       console.log(json)
+//       if (json.message === "User already signed up"){
+//         console.log("error")
+//         btn.disabled = true
+//         btn.innerHTML = "Submit"
+//         document.querySelector("#UMIFNf").style = "display:none;"
+//         document.querySelector("#YH7cBX").className = "_l79chJ"
+//         document.querySelector("#QwgFJH").innerHTML =
+//         "You have already signed up for early access."
+//         return
+//       }
+//       btn.disabled = true
+//       btn.disabled = false
+//       btn.innerHTML = "Submit"
+//       document.querySelector("#YH7cBX").className = "_y89x4g"
+//       document.querySelector("#QwgFJH").innerHTML =
+//         "You have successfully signed up for early access for the kwallet app. When the app is ready for testing, you will receive an email from us."
+//       document.querySelector("#UMIFNf").style = "display:none;"
+//     })
+//     .catch((error) => {
+//       btn.disabled = false
+//       btn.innerHTML = "Submit"
+//       document.querySelector("#UMIFNf").style = "display:none;"
+//       document.querySelector("#YH7cBX").className = "_l79chJ"
+//       document.querySelector("#QwgFJH").innerHTML =
+//         "An error occurred when trying to submit your email. Please refresh the page and try again."
+//     })
+// })
 
 var html = document.querySelector("html")
 var modal = document.getElementById("L5rthN")
